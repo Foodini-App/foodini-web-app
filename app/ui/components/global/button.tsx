dc25941f45549fc6b83539b0d0c153a11cee1ff1
Darren <dali5314@gmail.com>
2024-04-28 15:20:24 -0400

    hero

--- app/ui/components/global/button.tsx
+++ app/ui/components/global/button.tsx
@@ -6,7 +6,7 @@ import { X } from "lucide-react";
 import { cn } from "@/app/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-xl font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-lg md:text-xl font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300",
   {
     variants: {
       variant: {
--- app/ui/components/hero.tsx
+++ app/ui/components/hero.tsx
@@ -3,25 +3,28 @@ import { Button } from "./global/button";
 
 export default function Hero() {
   return (
-    <div className="relative">
-      <img
-        src="hero-mobile.jpg"
-        alt="Hero"
-        className="w-full h-full object-cover lg:hidden"
-      />
-      <img
-        src="hero-wide.jpg"
-        alt="Hero"
-        className="hidden lg:block w-full h-auto object-cover"
-      />
-      <Button
-        size="default"
-        variant="blue"
-        className="absolute bottom-12 left-4 md:bottom-12 md:left-24 lg:bottom-8 bg-blue-500 text-white px-6 py-4 rounded-md"
-        href="/food_events"
-      >
-        Find a Food Fair
-      </Button>
+    <div className="grid grid-cols-3">
+      <div className="col-span-2 place-content-center bg-orange-500 border-r-8 border-white">
+        <div className="py-4 md:py-8 px-4 md:px-24">
+          <div className="text-xl md:text-3xl lg:text-4xl font-semibold text-white pb-2 border-b-4 border-white">
+            Discover new foods with confidence
+          </div>
+          <div className="text-md md:text-lg lg:text-xl font-medium text-white pt-2 pb-4 lg:pb-12">
+            Foodini helps you try new cuisines at local food events
+          </div>
+          <Button
+            size="default"
+            variant="blue"
+            className="bg-blue-500 text-white px-6 py-4 rounded-md"
+            href="/food_events"
+          >
+            Find a Food Fair
+          </Button>
+        </div>
+      </div>
+      <div className="col-span-1 h-64 md:h-80 xl:h-64 flex place-contents-center">
+        <img src="hero-image.jpg" alt="Hero" className="h-full md:w-full object-cover" />
+      </div>
     </div>
   );
 }
